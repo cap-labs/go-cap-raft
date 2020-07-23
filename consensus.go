7ec451534316acfe6ee5b7e30f49cf884f6aff28
yayanyang <org.lemon@gmail.com>
2020-07-23 18:07:36 +0800

    update

--- consensus.go
+++ consensus.go
@@ -1,15 +1,26 @@
 package raft
 
 import (
-	"github.com/cap-labs/go-cap"
 	"github.com/libs4go/scf4go"
 	"github.com/libs4go/smf4go"
 )
 
 //go:generate protoc --proto_path=./proto-cap --go_out=plugins=grpc,paths=source_relative:. raft.proto
 
+// NetWork .
+type NetWork interface {
+	Connect(peerID string) (raftStateMachineClient, error)
+}
+
+// Storage .
+type Storage interface {
+	GetCluster() ([]string, error)
+	UpdateCluster([]string) error
+}
+
 type raftConsensus struct {
-	Stream cap.NetworkStreamClient `inject:"raft.network.stream"`
+	Network Network `inject:"raft.network"`
+	Storage Storage `inject:"raft.storage"`
 }
 
 // New create raft consensus object compatible smf4go api
